1f73d28c00a46003862107adf925bf1312c95266
ParsaAzimi23 <pazimi730@gmail.com>
2023-01-22 17:40:40 +0330

    react-router-dom@5.2.0 installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
-export default App;
